4cf44896d4c45992851f9c9474de4e82d468c125
caiki <marketing@letcode.com.br>
2024-08-13 10:06:56 -0300

    descrição galeria

--- src/components/data/galerrys.tsx
+++ src/components/data/galerrys.tsx
@@ -5,14 +5,12 @@ export const Galeria: GalleryType[] = [
         id: 1, name: 'Nebulosa de Hélix', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/NGC7293_%282004%29.jpg/1024px-NGC7293_%282004%29.jpg', description: `Nebulosa de Hélix, também conhecida como Nebulosa da Hélice, A Hélix ou NGC 7293 é uma nebulosa planetária localizada na constelação de Aquarius ou Aquário
         Também foi apelidada de "Olho de Deus" devido ao seu formato visto da Terra.
         Descoberta por Karl Ludwig Harding, provavelmente antes de 1824, essa nebulosa é uma das mais próximas da Terra.
-        Sua distância da Terra é de aproximadamente 700 anos - luz(215 parsec).Ela é muito parecida com a nebulosa do anel, e têm tamanho, idade e características físicas parecidos à nebulosa do Haltere, sendo diferentes apenas em suas proximidades e aparência de um ângulo equatorial`},
+        `},
     {
         id: 2, name: 'Andrômeda', url: 'https://upload.wikimedia.org/wikipedia/commons/5/57/M31bobo.jpg',
         description: `A galáxia de Andrômeda (Messier 31, NGC 224) é uma galáxia espiral localizada a cerca de 2,54 milhões de anos-luz de distância da Terra, na direção da constelação de Andrômeda.
  É a galáxia espiral mais próxima da Via Láctea e seu nome é derivado da constelação onde está situada, que, por sua vez, tem o nome derivado da princesa mitológica Andrômeda. 
  É a maior galáxia do Grupo Local, que também contém a Via Láctea, a galáxia do Triângulo e aproximadamente 30 outras menores.
- O levantamento feito pelo Telescópio Espacial Spitzer em 2006 determinou que sua massa é de aproximadamente ~0,8-1,5 × 1012 massas solares enquanto a Via Láctea tem uma massa estimada em 8,5 × 1011 massas solares. 
- Sua população estelar atinge aproximadamente 1 trilhão de estrelas enquanto a Via Láctea conta com algo entre 200 a 400 bilhões de estrelas.
  
 ` },
     {
@@ -22,25 +20,20 @@ export const Galeria: GalleryType[] = [
 ` },
 
     {
-        id: 4, name: 'Gliese 832c', url: 'https://lh3.googleusercontent.com/-z0p5Kc-0UQY/VoLDlcU_p2I/AAAAAAAAV3o/PVQ-zkT93-s/s72-c/ilustra%2525C3%2525A7%2525C3%2525A3o%252520do%252520exoplaneta%252520Gliese%252520832c_thumb%25255B4%25255D.png?imgmax=800', description: `Gliese 832 c (também conhecido como Gl 832 c ou GJ 832 c) é um planeta extrassolar localizado a cerca de 16 anos-luz de distância a partir da Terra, na constelação de Grus, que orbita a estrela Gliese 832, uma anã vermelha.
+        id: 4, name: 'Gliese 832c', url: 'https://lh3.googleusercontent.com/-z0p5Kc-0UQY/VoLDlcU_p2I/AAAAAAAAV3o/PVQ-zkT93-s/s72-c/ilustra%2525C3%2525A7%2525C3%2525A3o%252520do%252520exoplaneta%252520Gliese%252520832c_thumb%25255B4%25255D.png?imgmax=800', description: `Gliese 832 c é um planeta extrassolar localizado a cerca de 16 anos-luz de distância a partir da Terra, na constelação de Grus, que orbita a estrela Gliese 832, uma anã vermelha.
 O planeta tem um Índice de Similaridade com a Terra de 0,81, um dos mais altos índices de similaridade com a Terra para qualquer planeta extrassolar conhecido.
 Ele está situado dentro da zona habitável de sua estrela.
 Ele é um dos exoplanetas confirmados potencialmente habitáveis conhecidos mais próximos da Terra.
-O exoplaneta mais próximo potencialmente habitável é Proxima Centauri b, a 4,2 anos-luz de distância.
 ` },
 
     {
-        id: 5, name: 'Marte', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/800px-OSIRIS_Mars_true_color.jpg', description: `Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar, atrás apenas de Mercúrio. Batizado em homenagem a divindade romana da guerra, muitas vezes é descrito como o "Planeta Vermelho", porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.
-Marte é um planeta rochoso com uma atmosfera fina, com características de superfície que lembram tanto as crateras de impacto da Lua quanto vulcões, vales, desertos e calotas polares da Terra. O período de rotação e os ciclos sazonais de Marte são também semelhantes aos da Terra, assim como é a inclinação que produz as suas estações do ano. 
-
+        id: 5, name: 'Marte', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/800px-OSIRIS_Mars_true_color.jpg', description: `Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar, atrás apenas de Mercúrio. Batizado em homenagem a divindade romana da guerra, muitas vezes é descrito como o "Planeta Vermelho", porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.Marte é um planeta rochoso com uma atmosfera fina, com características de superfície que lembram tanto as crateras de impacto da Lua quanto vulcões, vales, desertos e calotas polares da Terra. 
 ` },
 
     { id: 6, name: 'Jupiter', url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/800px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg', description: `Júpiter é o maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol. Possui menos de um milésimo da massa solar, contudo tem 2,5 vezes a massa de todos os outros planetas em conjunto. É um planeta gasoso, junto com Saturno, Urano e Netuno. Estes quatro planetas são por vezes chamados planetas jupiterianos ou planetas jovianos, e são os quatro gigantes gasosos, isto é, que não são compostos primariamente de matéria sólida` },
 
     {
-        id: 7, name: 'Saturno', url: 'https://content.nationalgeographic.com.es/medio/2023/08/18/una-gran-tormenta-domina-el-hemisferio-norte-de-satruno_7b0bb361_PIA12826_230818114638_800x800.jpg', description: `Saturno é o sexto planeta a partir do Sol e o segundo maior do Sistema Solar atrás de Júpiter. Pertencente ao grupo dos gigantes gasosos, possui cerca de 95 massas terrestres e orbita a uma distância média de 9,5 unidades astronômicas.
-
-Possui um pequeno núcleo rochoso, circundado por uma espessa camada de hidrogênio metálico e hélio. A sua atmosfera, também composta principalmente de hidrogênio, apresenta faixas com fortes ventos, cuja energia provém tanto do calor recebido do Sol quanto da energia irradiada de seu centro. Entretanto, estas bandas possuem aspecto pouco proeminente, com coloração que varia do marrom ao amarelado, devido à espessa névoa que envolve o planeta, além das camadas de nuvens. Sazonalmente surgem grandes sistemas de tempestades, além de vórtices permanentes existentes nos polos.` },
+        id: 7, name: 'Saturno', url: 'https://content.nationalgeographic.com.es/medio/2023/08/18/una-gran-tormenta-domina-el-hemisferio-norte-de-satruno_7b0bb361_PIA12826_230818114638_800x800.jpg', description: `Saturno é o sexto planeta a partir do Sol e o segundo maior do Sistema Solar atrás de Júpiter. Pertencente ao grupo dos gigantes gasosos, possui cerca de 95 massas terrestres e orbita a uma distância média de 9,5 unidades astronômicas.Possui um pequeno núcleo rochoso, circundado por uma espessa camada de hidrogênio metálico e hélio. A sua atmosfera, também composta principalmente de hidrogênio, apresenta faixas com fortes ventos, cuja energia provém tanto do calor recebido do Sol quanto da energia irradiada de seu centro.` },
 
     { id: 8, name: 'Buraco Negro', url: 'https://s2-g1.glbimg.com/-HG-hFtiRfsAsYTcL4yNnMJ_s2Q=/0x0:1438x966/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/B/Q/wFenRKRAaDkgKaNIzaJQ/buraco-negro-m87..png', description: `Buraco negro é uma região do espaço-tempo em que o campo gravitacional é tão intenso que nada — nenhuma partícula ou radiação eletromagnética como a luz — pode escapar. A teoria da relatividade geral prevê que uma massa suficientemente compacta pode deformar o espaço-tempo para formar um buraco negro. O limite da região da qual não é possível escapar é chamado de horizonte de eventos. Embora o horizonte de eventos tenha um efeito enorme sobre o destino e as circunstâncias de um objeto que o atravessa, não tem nenhuma característica local detectável. De muitas maneiras, um buraco negro age como um corpo negro ideal, pois não reflete luz.` },
     { id: 9, name: 'Netuno', url: 'https://www.infoescola.com/wp-content/uploads/2008/04/netuno.jpg', description: `Netuno ou Neptuno (AO 1990: Netuno ou Neptuno) é o oitavo planeta do Sistema Solar, o último a partir do Sol desde a reclassificação de Plutão para a categoria de planeta anão, em 2006. Pertencente ao grupo dos gigantes gasosos, possui um tamanho ligeiramente menor que o de Urano, mas maior massa, equivalente a 17 massas terrestres. Netuno orbita o Sol a uma distância média de 30,1 unidades astronômicas.` },
